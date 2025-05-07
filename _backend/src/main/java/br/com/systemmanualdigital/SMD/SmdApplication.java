8af682c3abcda63143ec4193c864d2770a6b6be6
luismfaria <luisfaria@yanksolutions.com.br>
2025-05-07 12:00:30 -0300

    refactor: inclusao de anotacoes do spring para reconhecer os repositories,domains e components

--- _backend/src/main/java/br/com/systemmanualdigital/SMD/SmdApplication.java
+++ _backend/src/main/java/br/com/systemmanualdigital/SMD/SmdApplication.java
@@ -2,11 +2,18 @@ package br.com.systemmanualdigital.SMD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = "br.com.systemmanualdigital")
+@EntityScan(basePackages = {"br.com.systemmanualdigital.domains","br.com.systemmanualdigital.domains.enums"})
+@EnableJpaRepositories(basePackages = "br.com.systemmanualdigital.repositories")
 @SpringBootApplication
 public class SmdApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(SmdApplication.class, args);
 	}
 
