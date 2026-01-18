7d46450a7c9296eb7dd0fa2a7f63dc804f825634
lmateusfaria <luis.faria@fatec.sp.gov.br>
2026-01-18 15:22:27 -0300

    config: adiciona perfil 'prod' à classe DevConfig

--- _backend/src/main/java/br/com/systemmanualdigital/config/DevConfig.java
+++ _backend/src/main/java/br/com/systemmanualdigital/config/DevConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile({"dev", "prod"})
 public class DevConfig {
 
     @Autowired
